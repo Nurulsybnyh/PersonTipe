e02db88ae2bb63c8b2ca00ef4ac595e04eb7f3f7
Nurul sya'baniyah <nurul.syabaniyah@yahoo.com>
2013-09-20 11:05:05 +0700

    first commit

---
+++ person.java
@@ -0,0 +1,11 @@
+class Person
+{
+	public static void main(String[] nurul)
+	{
+	int x=10;
+	char y;
+	y='A';
+		System.out.println("Menampilkan variable x "+x);
+		System.out.println("Menampilkan variable y "+y);
+	}
+}
